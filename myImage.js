a7288fd292c460cf0ca56039d57b5e24ee426b0d
branchwag <branchleannelmb@gmail.com>
2024-10-12 11:11:08 -0400

    draws circles and centers canvas

--- myImage.js
+++ myImage.js
@@ -13,9 +13,23 @@ class MyImage {
 		const hue = Math.random() * 360;
 		this.ctx.fillStyle = `hsl(${hue}, 100%, 50%)`;
 		this.ctx.fillRect(0, 0, width, height);
+
+		this.ctx.fillStyle = `hsl(${(hue + 180) % 360}, 100%, 50%)`;
+		for (let x = width / 6; x < width; x += width / 3) {
+			for (let y = height / 6; y < height; y += height / 3) {
+				const radius = 100;
+				this.ctx.beginPath();
+				this.ctx.arc(x, y, radius, 0, 2 * Math.PI);
+				this.ctx.fill();
+			}
+		}
 	}
 
 	addTo(parent) {
 		parent.appendChild(this.canvas);
+	}
+
+	addClass(className) {
+		this.canvas.classList.add(className);
 	}
 }
